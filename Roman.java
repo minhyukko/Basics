119e1f1350bbf2d2ae7efb9b2131741210f1b7b1
minhyukko <57718746+minhyukko@users.noreply.github.com>
2024-01-25 11:46:55 -0500

    Update Roman.java

--- Roman.java
+++ Roman.java
@@ -1,4 +1,12 @@
 public class Roman {
+
+    /*
+     * Function Name: intToRoman
+     * Description: The function will receive an integer as an input and produce a roman numeral.
+     * Author: Minhyuk Ko
+     * Student ID: minhyukko
+     * Code Editor: Vim
+     */
     public static String intToRoman(int num) {
         int[] values = { 1000, 500, 100, 50, 10, 5, 1 };
         String[] symbols = { "M", "D", "C", "L", "X", "V", "I" };
