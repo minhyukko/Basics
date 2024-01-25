f45f11ecc93f3d7dfa7c039a2f74040a8ed3a3b3
EdwardShiung <edwardshiung@gmail.com>
2024-01-25 12:56:24 -0500

    Create new branch for Roman to Integer

--- Roman.java
+++ Roman.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Roman {
 
     /*
@@ -22,5 +25,45 @@ public class Roman {
 
         return result.toString();
     }
-}
+        /*
+     * Function Name: romanToInt Function
+     * Description: The function will transfer Roman Code to Integer. By adding parameter, it will return an integer.
+     * Author: Tian Yu (Edward) Shiung
+     * Student ID: stianyu99
+     * Code Editor: Visual Studio Code
+     */
+    public int romanToInt(String s) {
 
+        //Using HashMap to store letters and corrseponding values to solve this problem
+        Map<Character, Integer> dic = new HashMap<>();
+        dic.put('I',1);
+        dic.put('V',5);
+        dic.put('X',10);
+        dic.put('L',50);
+        dic.put('C',100);
+        dic.put('D',500);
+        dic.put('M',1000);
+
+        //Calculate the total
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i ++){
+
+            char curr = s.charAt(i);
+            char next = (i + 1 < s.length()) ? s.charAt(i + 1) : '\0';
+
+
+            int currValue = dic.get(curr);
+            int nextValue = (next != '\0') ? dic.get(next) : 0;
+
+            if (currValue < nextValue) {
+                total -= currValue;
+            } else {
+                total += currValue;
+            }
+        }
+
+        System.out.println(total);
+        return total;
+    }
+}
